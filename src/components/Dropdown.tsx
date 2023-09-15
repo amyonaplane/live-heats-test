834b83ef61f9cefcd2762030ff5efec13d23e977
Amy <amy_heinrich@hotmail.com>
2023-09-15 21:26:02 +1000

    chore - formatting

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -6,11 +6,26 @@ interface DropdownProps {
   disabled?: boolean;
 }
 
-export default function Dropdown({name, id, array, className, disabled}: DropdownProps) {
+export default function Dropdown({
+  name,
+  id,
+  array,
+  className,
+  disabled,
+}: DropdownProps) {
   return (
-    <select disabled={disabled} name={name} id={id} className={`${className} ${disabled ? 'opacity-50' : ''} text-sm p-3 w-[60px]`}>
+    <select
+      disabled={disabled}
+      name={name}
+      id={id}
+      className={`${className} ${
+        disabled ? "opacity-50" : ""
+      } text-sm p-3 w-[60px]`}
+    >
       {array.map((arrayItem, i) => (
-        <option key={i} value={i + 1}>{i + 1}</option>
+        <option key={i} value={i + 1}>
+          {i + 1}
+        </option>
       ))}
     </select>
   );
